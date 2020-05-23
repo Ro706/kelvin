d8c7b674827aaa158c34e1467fe63061df3dc8da
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-05-23 09:38:28 +0000

    create kelvin.py

---
+++ kelvin.py
@@ -0,0 +1,10 @@
+print "welcome in"
+def hello():
+    print "hello welcome in Ro706
+hello()
+print "1°c = 274.15°k"
+c = input ("c:")
+print "k = c + 274.15"
+k = c + 274.15
+print "k= " ,k "°k"
+print "thank u"
