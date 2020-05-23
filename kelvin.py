7863128d492a6d7df5eda8c7f27980ac0eaf89f4
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-05-23 18:36:55 +0000

    update kelvin.py
    By this u can calculate force also..

--- kelvin.py
+++ kelvin.py
@@ -1,10 +1,15 @@
 print "welcome in"
 def hello():
-    print "hello welcome in Ro706
+    print "hello welcome in Ro706"
 hello()
 print "1°c = 274.15°k"
 c = input ("c:")
-print "k = c + 274.15"
 k = c + 274.15
 print "k= " ,k "°k"
 print "thank u"
+def froce():
+    return m * g
+print "we know that F=mg"
+m = input("m:")
+g = input("g:")
+print "f=",force()
