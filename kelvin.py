aa5ad1d01e69e36873e3433545da35a9c659957d
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-05-25 04:23:27 +0000

    update kelvin.py

--- kelvin.py
+++ kelvin.py
@@ -1,8 +1,8 @@
 print "welcome in "
 def hello():
-    print "hello welcome in Ro706"                                 hello()
-c = input ("c:")                                                   k = c + 274.15
-print "k= " ,k                                                     print "thank u"
+    print "hello welcome in Ro706" 
+c = input ("c:")                                                   
+print "k= " ,k                                                     
 print "we know that F=mg"
 m = input("m:")
 g = input("g:")
